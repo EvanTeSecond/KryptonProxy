6536ff1dd51415127f2b9c0cfcdf14189bb470f9
evan <coolmineralsandfossils@Gmail.com>
2025-03-15 23:20:42 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
 // public/sw.js
-importScripts('/uv/uv.bundle.js');
+/*importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
-importScripts('/uv/uv.sw.js');
+importScripts('/uv/uv.sw.js');*/
+import { UVServiceWorker } from '/uv/uv.bundle.js';
+import { UVServiceWorker } from '/uv/uv.config.js';
+import { UVServiceWorker } from '/uv/uv.sw.js';
 
 const uv = new UVServiceWorker();
 self.addEventListener('fetch', (event) => {
